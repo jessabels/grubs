97000f7fe9f3d63d9a3adc3682dc9d4c71c58e25
Jessica Abels <jessabels92@gmail.com>
2020-12-09 10:47:07 -0800

    Add user dropdown to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,8 +3,8 @@ import { Link, NavLink, withRouter } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Avatar from "@material-ui/core/Avatar";
 import HomeIcon from "@material-ui/icons/Home";
+import { Avatar, Popover, List, ListItem } from "@material-ui/core";
 
 import "./Navbar.css";
 import Logout from "./auth/Logout";
@@ -14,31 +14,90 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "space-between",
   },
+  listItem: {
+    borderTop: "1px solid #f2f2f2",
+    cursor: "pointer",
+
+    "&:hover": {
+      background: "#e5e5e5",
+    },
+  },
+  avatar: {
+    cursor: "pointer",
+  },
 }));
 
 const Navbar = (props) => {
   const classes = useStyles();
-  return (
-    <div className="navbar-container">
-      <AppBar className={classes.navbar} position="static">
-        <Toolbar className={classes.navbar}>
-          <div className="logo-info">
-            {props.location.pathname === "/profile" ? (
-              <NavLink to="/">
-                <HomeIcon />{" "}
-              </NavLink>
-            ) : null}
-          </div>
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-          <div className="user-info" style={{ display: "flex" }}>
-            <Link to="/profile">
-              <Avatar alt="user avatar"></Avatar>
-            </Link>
-            {!props.needLogin ? <Logout /> : null}
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return (
+    <>
+      <div className="navbar-container">
+        <AppBar className={classes.navbar} position="static">
+          <Toolbar className={classes.navbar}>
+            <div className="logo-info">
+              {props.location.pathname === "/profile" ? (
+                <NavLink to="/">
+                  <HomeIcon />
+                </NavLink>
+              ) : null}
+            </div>
+            {!props.needLogin ? (
+              <div className="user-info" style={{ display: "flex" }}>
+                <Avatar
+                  className={classes.avatar}
+                  onClick={handleClick}
+                  alt="user avatar"
+                ></Avatar>
+              </div>
+            ) : null}
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <List>
+            <ListItem className={classes.listItem}>
+              <Link
+                style={{ textDecoration: "none", color: "black" }}
+                to="/profile"
+                onClick={handleClose}
+              >
+                Saved Recipes
+              </Link>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Logout handleClose={handleClose} />
+            </ListItem>
+          </List>
+        </Popover>
+      </div>
+    </>
   );
 };
 
--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../store/actions/entities";
 
-const Logout = () => {
+const Logout = (props) => {
   const handleClick = () => {
+    props.handleClose();
     dispatch(logout());
   };
 
   const dispatch = useDispatch();
-  return <button onClick={handleClick}>Logout</button>;
+  return <span onClick={handleClick}>Logout</span>;
 };
 
 export default Logout;
